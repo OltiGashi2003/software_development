6384e81d3c80fad01b52e40379ec7c61d946cfa4
Olti Gashi <ogashi@thequantix.com>
2023-03-24 11:39:26 +0100

    first version

---
+++ taschenrechner.py
@@ -0,0 +1,12 @@
+number_1 = float(input("enter please the first number: "))
+number_2 = float(input("enter please the second number: "))
+op = (input("enter please the op: "))
+
+if op == "+":
+    print(number_1 + number_2)
+elif op == "-":
+    print(number_1 - number_2)
+elif op == "/":
+    print(number_1 / number_2)
+elif op == "*":
+    print(number_1 * number_2)
